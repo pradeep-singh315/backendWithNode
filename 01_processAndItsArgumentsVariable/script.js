a7b484f6ae68d0c8d25d30062a5c3c7a0ee180c9
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-12-12 05:48:57 +0530

    learn about process

---
+++ 01_processAndItsArgumentsVariable/script.js
@@ -0,0 +1,13 @@
+const vegetable = "Potato"
+
+const sum = (a, b) => {
+    return a+b
+}
+
+const ans = sum(8, 12)
+console.log(ans);
+
+console.log(process);
+
+const arrayProcess = process.argv
+console.log(arrayProcess);
