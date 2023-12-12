870c72f541aed1d461e01cf4fb10ffa139a416ed
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-12-12 06:19:19 +0530

    works on argument variables

--- 01_processAndItsArgumentsVariable/script.js
+++ 01_processAndItsArgumentsVariable/script.js
@@ -11,3 +11,8 @@ console.log(process);
 
 const arrayProcess = process.argv
 console.log(arrayProcess);
+
+for(i = 2; i < arrayProcess.length; i++) // ----> Because first two values are fixed
+{
+    console.log(`Hello to ${arrayProcess[i]}`);
+}
