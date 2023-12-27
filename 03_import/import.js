5190b12c1cb79540e8ba85e79774c0fe6f3cd4c5
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-12-27 05:50:23 +0530

    generate new words....

--- 03_import/import.js
+++ 03_import/import.js
@@ -4,3 +4,13 @@ const ans = sum(9, 11)
 console.log(ans);
 console.log(piValue);
 
+// Generating new words..
+
+import { generate } from "random-words";
+
+//generate() -----> Generate new words...
+
+for(let i = 0; i <= 10; i++)
+{
+    console.log(generate());
+}
