59c1c0982990e50cca2df5007f0da4f6fed0953b
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-12-27 05:45:19 +0530

    read about import....

---
+++ 03_import/import.js
@@ -0,0 +1,6 @@
+import {piValue, sum} from "./mathFile.js"
+
+const ans = sum(9, 11)
+console.log(ans);
+console.log(piValue);
+
---
+++ 03_import/mathFile.js
@@ -0,0 +1,3 @@
+export const sum = (a,b) => a + b
+
+export const piValue = 3.14
